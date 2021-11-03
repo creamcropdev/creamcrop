97f1ce86c7271be1cd823b9140225f9c5712dc73
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-11-02 18:01:21 -0700

    Fix about command

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -3,4 +3,4 @@ const require = createRequire(import.meta.url);
 let pkg = require("../../package.json");
 
 export var version = pkg.version
-export var metadata =  `${pkg.name}\n${pkg.description}\n${pkg.author}\n${pkg.version}`
+export var about =  `${pkg.name}\n${pkg.description}\n${pkg.author}\n${pkg.version}`
